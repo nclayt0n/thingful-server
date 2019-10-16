ba74c3d58b84a3a7382b069714d44aa0817ec7a6
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-10-15 21:51:40 -0400

    adding jwt

--- src/reviews/reviews-service.js
+++ src/reviews/reviews-service.js
@@ -1,17 +1,17 @@
 const xss = require('xss')
 
 const ReviewsService = {
-  getById(db, id) {
-    return db
-      .from('thingful_reviews AS rev')
-      .select(
-        'rev.id',
-        'rev.rating',
-        'rev.text',
-        'rev.date_created',
-        'rev.thing_id',
-        db.raw(
-          `row_to_json(
+    getById(db, id) {
+        return db
+            .from('thingful_reviews AS rev')
+            .select(
+                'rev.id',
+                'rev.rating',
+                'rev.text',
+                'rev.date_created',
+                'rev.thing_id',
+                db.raw(
+                    `row_to_json(
             (SELECT tmp FROM (
               SELECT
                 usr.id,
@@ -22,38 +22,38 @@ const ReviewsService = {
                 usr.date_modified
             ) tmp)
           ) AS "user"`
-        )
-      )
-      .leftJoin(
-        'thingful_users AS usr',
-        'rev.user_id',
-        'usr.id',
-      )
-      .where('rev.id', id)
-      .first()
-  },
+                )
+            )
+            .leftJoin(
+                'thingful_users AS usr',
+                'rev.user_id',
+                'usr.id',
+            )
+            .where('rev.id', id)
+            .first()
+    },
 
-  insertReview(db, newReview) {
-    return db
-      .insert(newReview)
-      .into('thingful_reviews')
-      .returning('*')
-      .then(([review]) => review)
-      .then(review =>
-        ReviewsService.getById(db, review.id)
-      )
-  },
+    insertReview(db, newReview) {
+        return db
+            .insert(newReview)
+            .into('thingful_reviews')
+            .returning('*')
+            .then(([review]) => review)
+            .then(review =>
+                ReviewsService.getById(db, review.id)
+            )
+    },
 
-  serializeReview(review) {
-    return {
-      id: review.id,
-      rating: review.rating,
-      text: xss(review.text),
-      thing_id: review.thing_id,
-      date_created: review.date_created,
-      user: review.user || {},
+    serializeReview(review) {
+        return {
+            id: review.id,
+            rating: review.rating,
+            text: xss(review.text),
+            thing_id: review.thing_id,
+            date_created: review.date_created,
+            user: review.user || {},
+        }
     }
-  }
 }
 
 module.exports = ReviewsService
--- test/things-endpoints.spec.js
+++ test/things-endpoints.spec.js
@@ -12,8 +12,8 @@ describe('Things Endpoints', function() {
     } = helpers.makeThingsFixtures()
 
     before('make knex instance', () => {
-        db = knex({
-            client: 'pg',
+       db = knex({
+            client:'pg',
             connection: process.env.TEST_DB_URL,
         })
         app.set('db', db)
